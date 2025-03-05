8adb0d7749bd8a6d8605a91ab65c5129184703cb
kanchansingh2004 <kannursingh2004@gmail.com>
2025-03-05 12:52:15 +0530

    [Kanchan] Adding AddressBook and Contact file

---
+++ AddressBook/addressBook.js
@@ -0,0 +1,14 @@
+//Create AddressBook class with constructor
+class AddressBook{
+    //Constructor to initialize values
+    constructor(firstName, LastName, address, city, state, zip, phone, email){
+        this.firstName = firstName;
+        this.LastName = LastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
